bd0b9c2299a16349ca949a696d8b457b01296ece
INW-TECH\ricardo.trindade <ricardo.trindade@innowave.tech>
2018-10-03 15:35:25 +0100

    todo-app/effects done

--- src/state/counter/counter.actions.ts
+++ src/state/counter/counter.actions.ts
@@ -1,8 +1,23 @@
 import { Action } from '@ngrx/store';
 
+import { State } from './counter.state';                // add
+
 export enum CounterActionTypes {
     INCREMENT = '[COUNTER] Increment',
     DECREMENT = '[COUNTER] Decrement',
+    LOAD = '[COUNTER] Load',                            // add
+    LOAD_SUCCEEDED = '[COUNTER] Load Succeeded',        // add
+    LOAD_FAILED = '[COUNTER] Load Failed',              // add
+}
+
+// (..)
+
+export class Load implements Action {                   // add
+    readonly type = CounterActionTypes.LOAD;            // add
+}                                                       // add
+export class LoadSucceeded implements Action {          // add
+    readonly type = CounterActionTypes.LOAD_SUCCEEDED;  // add
+    constructor(public payload: {counter: number}) { }  // add
 }
 
 export class Increment implements Action {
@@ -13,6 +28,9 @@ export class Decrement implements Action {
     readonly type = CounterActionTypes.DECREMENT;
 }
 
+
 export type CounterActions
     = Increment
-    | Decrement;
+    | Decrement
+    | Load                                              // add
+    | LoadSucceeded;
