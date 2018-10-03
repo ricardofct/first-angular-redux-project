1cbd7296c4eaa450d8544a66629c6fc889a2c7b9
INW-TECH\ricardo.trindade <ricardo.trindade@innowave.tech>
2018-10-03 10:28:02 +0100

    step 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { environment } from '@env/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tasteful-angular';
+  constructor() {
+    console.log({ environment });
+  }
 }
